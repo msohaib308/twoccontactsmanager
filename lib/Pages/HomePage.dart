bdbb0edc14a9f26648bd661ea1c9881579434d70
Sohaib <msohaib308@gmail.com>
2020-10-03 16:28:54 +0500

    display contacts

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
+
 import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
-
-import '../chatList.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,7 +13,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Contact> contactList = [];
-  String _name = '';
   @override
   void initState() {
     super.initState();
@@ -23,11 +23,21 @@ class _HomePageState extends State<HomePage> {
     final PermissionStatus permissionStatus = await _getPermission();
     if (permissionStatus == PermissionStatus.granted) {
       //We can now access our contacts here
-      List<Contact> contacts = (await ContactsService.getContacts()).toList();
+      List<Contact> contacts =
+          (await ContactsService.getContacts(withThumbnails: false)).toList();
+
       // debugPrint(contacts[0].displayName);
       setState(() {
         contactList = contacts;
       });
+
+      // Lazy load thumbnails after rendering initial contacts.
+      for (final contact in contactList) {
+        ContactsService.getAvatar(contact).then((avatar) {
+          if (avatar == null) return; // Don't redraw if no change.
+          setState(() => contact.avatar = avatar);
+        });
+      }
     } else {
       //If permissions have been denied show standard cupertino alert dialog
       showDialog(
@@ -63,12 +73,18 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Contacts List')),
-        // leading: GestureDetector(
-        //   onTap: () {},
-        //   child: Icon(CupertinoIcons.photo_camera),
-        // ),
-      ),
+          title: Center(
+              child: Text('Contacts List ' + contactList.length.toString())),
+          // leading: GestureDetector(
+          //   onTap: createCOntact,
+          //   child: Icon(CupertinoIcons.add_circled),
+          // ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.create),
+              onPressed: createCOntact,
+            )
+          ]),
       // body: HomePageWidgets.pageStyle1(),
       body: Container(
         child: Column(
@@ -91,7 +107,7 @@ class _HomePageState extends State<HomePage> {
               shrinkWrap: true,
               itemCount: contactList.length,
               itemBuilder: (context, index) {
-                Contact contact = contactList?.elementAt(index);
+                Contact contact = contactList[index];
                 return Column(
                   children: [
                     ListTile(
@@ -101,11 +117,14 @@ class _HomePageState extends State<HomePage> {
                               backgroundImage: MemoryImage(contact.avatar),
                             )
                           : CircleAvatar(
-                              child: Text(contact.initials()),
+                              child: Text(contact.initials().toString()),
                               backgroundColor: Theme.of(context).accentColor,
                             ),
-                      title: Text(contact.displayName),
-                      subtitle: Text(contact.phones.single.value),
+                      title: Text(contact.displayName ?? ''),
+                      subtitle: Text(contact?.phones?.firstWhere(
+                          (element) => element.value.isNotEmpty, orElse: () {
+                        return Item(value: '');
+                      })?.value),
                       // trailing: Text('6:45 pm'),
                       trailing: Wrap(
                         spacing: 10, // space between two icons
@@ -116,7 +135,14 @@ class _HomePageState extends State<HomePage> {
                               color: Colors.green,
                               size: 30,
                             ),
-                            onPressed: () {},
+                            onPressed: () {
+                              // var sds = index;
+                              // _launchURL();
+                              launchWhatsApp(
+                                  contact: contact,
+                                  message: 'Allah is the best planner',
+                                  context: context);
+                            },
                           ), // icon-1
                           IconButton(
                             icon: Icon(
@@ -147,9 +173,59 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // _launchURL() async {
+  //   const url = 'https://flutter.dev';
+  //   if (await canLaunch(url)) {
+  //     await launch(url);
+  //   } else {
+  //     throw 'Could not launch $url';
+  //   }
+  // }
+
+  void createCOntact() async {
+    await ContactsService.openContactForm();
+    getAllContacts();
+  }
+
+  void launchWhatsApp({
+    @required Contact contact,
+    @required String message,
+    @required BuildContext context,
+  }) async {
+    // var phone = '';
+    // var phone = contact.phones.toString();
+    String myUrl = '';
+    if (Platform.isIOS) {
+      myUrl = "whatsapp://wa.me/+923473027259/?text=${Uri.parse(message)}";
+    } else {
+      myUrl = "whatsapp://send?phone=+923473027259&text=${Uri.parse(message)}";
+    }
+
+    if (await canLaunch(myUrl)) {
+      // String data = "content://com.android.contacts/data/" + dataId;
+      // String type = "vnd.android.cursor.item/vnd.com.whatsapp.profile";
+      // Intent sendIntent = new Intent();
+      // sendIntent.setAction(Intent.ACTION_VIEW);
+      // sendIntent.setDataAndType(Uri.parse(data), type);
+      // sendIntent.setPackage("com.whatsapp");
+      // startActivity(sendIntent);
+      // if (mimeType.equals("vnd.android.cursor.item/vnd.com.whatsapp.voip.call") || mimeType.equals("vnd.android.cursor.item/vnd.com.whatsapp.video.call"))
+      await launch(myUrl);
+    } else {
+      // throw 'Could not launch ${myUrl}';
+      Scaffold.of(context).showSnackBar(SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text('Could not launch $myUrl'),
+        backgroundColor: Colors.orange,
+        elevation: 6.0,
+      ));
+    }
+  }
+
   void filterContacts(String query) async {
     List<Contact> contacts =
-        (await ContactsService.getContacts(query: query)).toList();
+        (await ContactsService.getContacts(query: query, withThumbnails: false))
+            .toList();
     // debugPrint(contacts[0].displayName);
     setState(() {
       contactList = contacts;
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: '2C Contacts',
       theme: ThemeData(
         brightness: Brightness.light,
         primaryColor: Colors.purple[800],
